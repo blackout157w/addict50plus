5151d8eb7ecc68deb40ea692062c912178ec7b5e
blackout <97791357+blackout157w@users.noreply.github.com>
2025-01-05 15:29:25 +0900

    Add files via upload

---
+++ addict50plus/config.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA0, 0x89, 0x37, 0x15, 0xF6, 0x40, 0x1A, 0x34}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
